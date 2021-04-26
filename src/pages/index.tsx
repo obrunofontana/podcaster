c84802132e8a7c9d1f9662de44bb534bb3e077db
Bruno Fontana <obrunofontana@gmail.com>
2021-04-26 20:30:44 -0300

    implementa chamadas atrasves das tres estrateias spa, ssr e ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+// SPA
+// SSR
+// SSG
 
-export default function Home() {
+export default function Home(props) {
+  console.log('lokinho meu ', props.episodes)
   return <h1>home</h1>;
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3333/episodes');
+  const data = await res.json();
+
+  console.log('data -> ', data);
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8  // A cada 8 horas é realizada uma nova chamada pra API
+  };
 }
